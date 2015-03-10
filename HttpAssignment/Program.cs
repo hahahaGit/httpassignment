272017b8070580d0b8b971337ca8c3cb26d460cf
Rajesh Hamal <chhetriasispkr@gmail.com>
2015-03-11 00:20:40 +0100

    Static Reply

---
+++ HttpAssignment/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HttpAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TcpListener serverSocket = new TcpListener(8888);
+            serverSocket.Start();
+            while (true)
+            {
+                TcpClient connectionSocket = serverSocket.AcceptTcpClient();
+                Console.WriteLine("Server activated now");
+                Server server = new Server(connectionSocket);
+                 Task.Run(() => server.SimpleReply());
+            }
+        }
+    }
+}
---
+++ HttpAssignment/Server.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HttpAssignment
+{
+    class Server
+    {
+        private TcpClient connectionSocket;
+
+        public Server(TcpClient connectionSocket)
+        {
+            
+            this.connectionSocket = connectionSocket;
+        }
+
+        internal void SimpleReply()
+        {
+
+            
+            try
+            {
+                  Stream ns = connectionSocket.GetStream();
+            StreamReader sr = new StreamReader(ns);
+            StreamWriter sw = new StreamWriter(ns);
+            sw.AutoFlush = true; // enable automatic flushing
+
+            string message = sr.ReadLine();
+            string answer;
+            while (message != null && message != "")
+            {
+                //Console.WriteLine("Client: " + message);
+                answer = message;
+                sw.WriteLine(answer);
+                message = sr.ReadLine();
+
+            }
+            ns.Close();
+            connectionSocket.Close();
+        }
+            catch (SocketException se)
+            {
+
+                Console.WriteLine(se.Message);
+            }
+            catch (IOException ioe)
+            {
+                Console.WriteLine(ioe.Message);
+            }
+        }
+
+         
+        }
+       
+    }
+
